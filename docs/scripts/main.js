4daa6f3439f69fc29e70faef20c9ce0d13497b98
jrabbit2001 <119432775+jrabbit2001@users.noreply.github.com>
2022-11-30 00:55:56 +0900

    Delete main.js

--- docs/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-$(document).ready(function () {
-    $('#go-to-top').click(function () {
-        $('html,body').animate({scrollTop: 0}, 400);
-        return false;
-    });
-})
